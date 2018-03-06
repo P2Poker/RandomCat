2d4ff33d7dacab50d0557cbb73c93e98888b08d7
P2Poker <P2Poker@outlook.com>
2018-03-06 16:40:55 -0700

    Removed RandomCatCipher.h

--- RandomCat/TrueRandomCat.h
+++ RandomCat/TrueRandomCat.h
@@ -3,7 +3,10 @@
 
 
 
-#include "RandomCatCipher.h"
+#include "RandomCatCommon.h"
+
+// AES standard block cipher
+#include <aes.h> // Universal interface provided by Crypto++ library
 
 // OS-implemented cryptographic quality pseudo-RNG
 #include <osrng.h> // Universal interface provided by Crypto++ library
