b13a6382a54206c72dca8e5688b792f2182ff2d3
P2Poker <P2Poker@outlook.com>
2018-03-08 11:18:20 -0700

    Lowercased filenames, added comments

--- RandomCat/PseudoRandomCat.h
+++ RandomCat/PseudoRandomCat.h
@@ -1,8 +1,12 @@
+
+/* If base class not included yet, include base class first.
+	RandomCat.h will re-include this header after the base class definition. */
 #ifndef RANDOMCAT_RANDOMCAT_H
 
-	#include "RandomCat.h"
+	// High-performance multi-threaded crypto-secure true RNG
+	#include "randomcat.h"
 
-#elif !defined RANDOMCAT_PSEUDORANDOMCAT_H
+#elif !defined RANDOMCAT_PSEUDORANDOMCAT_H  // Traditional macro guard
 #define RANDOMCAT_PSEUDORANDOMCAT_H
 
 
@@ -11,7 +15,8 @@
 
 
 
-class PseudoRandomCat
+class PseudoRandomCat :
+	public RandomCat
 {
 } ;
 
